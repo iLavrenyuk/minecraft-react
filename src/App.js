1e7c00c4197e53d8279d40449e3848593d76f5a6
iLavrenyuk <illya.lav@gmail.com>
2020-10-09 23:23:58 +0300

    Добавил плоскость, тени, траву

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import {Canvas} from 'react-three-fiber';
-import {Sky} from 'drei';
-import {Vector3} from 'three';
-import {Physics} from 'use-cannon'
+import { Canvas } from 'react-three-fiber';
+import { Sky } from 'drei';
+import { Vector3 } from 'three';
+import { Physics } from 'use-cannon';
+import { Ground } from './Ground'
 
 
 function App() {
-  return(
+  return (
     <Canvas>
-      <Sky sunPosition={new Vector3(100, 10, 100)}/>
-      <ambientLight intensity={0.3}/>
+      <Sky sunPosition={new Vector3(100, 10, 100)} />
+      <ambientLight intensity={0.3} />
       <pointLight
-      castShadow
-      intensity={0.8}
-      position={[100, 100, 100]}
+        castShadow
+        intensity={0.8}
+        position={[100, 100, 100]}
       />
       <Physics gravity={[0, -30, 0]}>
-
+        <Ground />
       </Physics>
     </Canvas>
   )
---
+++ src/Ground.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import grass from 'img/grass'
+import { RepeatWrapping, TextureLoader } from 'three';
+
+
+export const Ground = (props) => {
+    // Добавляю физические свойства земле
+    //Специальный хук из пакета Cannon
+    const [ref] = usePlane(() => ({
+        rotation: [-Math.PI / 2, 0, 0], ...props //Поворачиваем Plane горизонтально
+    }))
+
+    const texture = new TextureLoader.load(grass);
+    //Повторяю текстуру
+    texture.wrapS = RepeatWrapping;
+    texture.wrapT = RepeatWrapping;
+    // Сколько раз ей нужно повториться
+    texture.repeat.set(240, 240)
+
+
+    return (
+        //3-d element (three.js library)
+        //Чтобы в плоскости на земле рисовались тени
+        <mesh ref={ref} receiveShadow>
+             //Плоскость
+            <planeBufferGeometry attach="geometry" args={[1000, 1000]} />
+            // Teкстура которую использую
+            <meshStandardMaterial map={texture} attach="material" />
+        </mesh>
+    )
+}
