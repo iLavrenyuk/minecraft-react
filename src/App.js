7a212eed182aeb9b3957eee08defb47c7ef791b9
iLavrenyuk <illya.lav@gmail.com>
2020-10-09 23:35:50 +0300

    Add Camera Plus def cam

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Canvas } from 'react-three-fiber';
 import { Sky } from 'drei';
 import { Vector3 } from 'three';
 import { Physics } from 'use-cannon';
-import { Ground } from './Ground'
+import { Ground } from './Ground';
+import { Camera } from './Camera'
 
 
 function App() {
   return (
     <Canvas>
+      // Отрисовка мира от лица персонажа, при помощи Камеры
+      <Camera />
       <Sky sunPosition={new Vector3(100, 10, 100)} />
       <ambientLight intensity={0.3} />
       <pointLight
